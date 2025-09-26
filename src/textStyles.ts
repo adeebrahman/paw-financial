05f09b9966a8976d9feb0896ac0355713253b653
Adeeb Rahman <adeeb167@gmail.com>
2025-09-25 21:18:50 -0400

    merged file restructure issues

---
+++ src/textStyles.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Title1 = styled.p`
+	font-size: 20px;
+	font-weight: 700;
+`;
+
+export const Title2 = styled.p`
+	font-size: 16px;
+	font-weight: 700;
+`;
+
+export const Label = styled.p`
+	font-weight: 500;
+	font-size: 14px;
+`
+
+export const SecondaryText = styled.p`
+	font-size: 12px;
+	color: #8c8b8b;
+`
+
+export const ErrorMessage = styled.p`
+	font-size: 14px;
+	color: #be1a1aff;
+`
